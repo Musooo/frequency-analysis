b6db348e1816bb01fb8ae2dae17a319b5e9898b8
Musooo <nicoloblack2@gmail.com>
2024-10-31 16:20:00 +0100

    table printing better

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
-    "github.com/musooo/frequency-analysis/internal"
-    "github.com/musooo/frequency-analysis/pkg"
+	"os"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/musooo/frequency-analysis/internal"
+	"github.com/musooo/frequency-analysis/pkg"
 )
 
 func main() {
@@ -12,10 +14,18 @@ func main() {
     internal.RelativeFrequency(&arr.Fs,count,&arr.RelativeF)
     internal.IsCalc(&arr.RelativeF,&arr.Is)
     entropyTot := internal.EntropyCalc(&arr.RelativeF,&arr.Is, &arr.Hs)
-    fmt.Println("S F(s) f(s) H(s)")
+    t := table.NewWriter()
+    t.SetOutputMirror(os.Stdout)
+    t.AppendHeader(table.Row{"S", "F(s)", "'f'(s)","I(s)", "H(s)"})
     for key,value := range arr.Fs {
-		fmt.Println(key, " ",value, arr.RelativeF[key], " ", arr.Is[key] , " ", arr.Hs[key])
+        t.AppendRow(table.Row{key,value,arr.RelativeF[key], arr.Is[key], arr.Hs[key]})
+		//fmt.Println(key, " ",value, arr.RelativeF[key], " ", arr.Is[key] , " ", arr.Hs[key])
 	}
-    fmt.Println("tot ", count, " 1 ", "/ ", entropyTot)
+    //fmt.Println("tot ", count, " 1 ", "/ ", entropyTot)
+    t.AppendRow(table.Row{"tot",count,"1","/",entropyTot})
+
+    t.SetStyle(table.StyleLight)
+
+    t.Render()
 
 }
