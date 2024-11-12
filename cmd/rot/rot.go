edafe4c4a5c7ca9064760a35f00906cb871cae2b
Musooo <nicoloblack2@gmail.com>
2024-11-12 18:43:24 +0100

    you can choose to use go routine

--- cmd/rot/rot.go
+++ cmd/rot/rot.go
@@ -5,8 +5,10 @@ import (
 	"github.com/musooo/frequency-analysis/pkg"
 )
 
-func rotN(text *string, num int) string {
+func rotN(text *string, num int, ) string { //wg *sync.WaitGroup
+    //defer wg.Done()
     var outS string
+    outS = fmt.Sprintf("rot:%d\n", num)
     for _, char := range *text {
         if char >= 'A' && char <= 'Z' {
             shifted := ((int(char)-'A')+num)%26 + 'A'
@@ -18,14 +20,22 @@ func rotN(text *string, num int) string {
             outS += string(char)
         }
     }
+    outS += ("\n------------\n")
+
     return outS
 }
 
 func Rot(){
 	str := pkg.ReadFileAndReturnString("file.txt")
-	fmt.Println(rotN(&str,12))
+	//fmt.Println(rotN(&str,12))
+    // var wg sync.WaitGroup
 	// for i:=1; i<27; i++{
-	// 	fmt.Println(rotN(&str,i))
-	// 	fmt.Println("-----------")
+    //     wg.Add(1)
+    //     go rotN(&str,i, &wg)
 	// }
+
+    // wg.Wait()
+    for i:=1; i<27; i++{
+        fmt.Println(rotN(&str,i))
+    }
 }
